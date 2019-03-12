2e3acf61a81f297015b4f982fa5ab68ef6140aea
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-12 17:26:56 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay] [:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     return programmer_hash[:dennis_ritchie][:languages] = "C"
+     
 end
 
 def adding_matz
@@ -80,7 +86,7 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash = {:yukihiro_matsumoto => {:known_for => "Ruby"}}
 end
 
 def changing_alan
@@ -102,8 +108,11 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+ 
+     return programmer_hash
 end
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -121,5 +130,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:dennis_ritchie][:languages] = "C", "Assembly"
+    
+    return programmer_hash
 end
